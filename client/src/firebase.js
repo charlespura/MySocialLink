8210ca81f95b1c51dfd810d01403b95cbe5c10ce
Charles Pura <164540352+charlespura@users.noreply.github.com>
2026-02-18 00:20:09 +0800

    add gitignore

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyApo7T486mxPBuFFAOTKa13ktiDoz_Ww-4",
+  authDomain: "act1maamrich.firebaseapp.com",
+  projectId: "act1maamrich",
+  storageBucket: "act1maamrich.firebasestorage.app",
+  messagingSenderId: "722768316793",
+  appId: "1:722768316793:web:bcef9477e003349c7807a8",
+  measurementId: "G-JRPXB6C4ZF"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
